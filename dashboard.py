933cc66ed3015d42ad19a6b376d8b5cfec1d0dea
abhiram haridas <abhiramharidas@gmail.com>
2017-09-14 23:52:31 +0530

    Implemented Edit About Us

--- dashboard.py
+++ dashboard.py
@@ -68,12 +68,59 @@ def edit_home():
 			print("Invalid Input")
 
 
+
+def edit_about():
+
+	db_ref_string = 'about'
+	db_ref = db.reference(db_ref_string)
+
+	while(1):
+
+		print("\nEnter 1 to edit About Us")
+		print("Enter 2 to edit Vision")
+		print("Enter 3 to edit How we Work")
+		print("Enter 4 to edit Join us")
+		print("Enter 5 to exit")
+	
+		choice = raw_input()
+		choice = int(choice)	
+	
+		if(choice == 1):
+			html = raw_input('Enter about html : ')
+			db_ref.update({
+				'about' : html
+				})
+		elif(choice == 2):
+			html = raw_input('Enter vision html : ')
+			db_ref.update({
+				'vision' : html
+				})
+		elif(choice == 3):
+			html = raw_input('Enter how we work html : ')
+			db_ref.update({
+				'how' : html
+				})
+		elif(choice == 4):
+			html = raw_input('Enter join us html : ')
+			db_ref.update({
+				'join_us' : html
+				})
+		elif(choice == 5):
+			return
+		else :
+			print("Invalid input")
+
+
 print ("Welcome to the Admin console!\n\n")
 
-print ("Enter 1 to edit Home Page\n")
+print ("Enter 1 to edit Home Page")
+print ("Enter 2 to edit About Page")
+
 choice = raw_input()
 choice = int(choice)
 
 
 if(choice == 1):
 	edit_home()
+elif(choice == 2):
+	edit_about()
